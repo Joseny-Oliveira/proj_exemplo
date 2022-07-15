ec414a9307e1d68d65a87387aa004d258c4acf55
josenyds@gmail.com <josenyds@gmail.com>
2022-07-15 20:39:14 -0300

    "Comentario inicial"

--- R/modelo.R
+++ R/modelo.R
@@ -2,11 +2,33 @@ mtcars <- mtcars
 
 modelo <- lm(mpg ~wt, data = mtcars)
 
+## Configurando o Token.
+
+
+library(usethis)
+library(gitcreds)
+library(credentials)
+library(gh)
+
+install.packages("gh")
+install.packages(c("gitcreds","credentials"))
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+gh::gh_whoami()
+#gitcreds::gitcreds_get()$password
+
+
 ## Passo 1 : crie e configure seu projeto.
+library(usethis)
 
 usethis::create_project()
 
-usethis::use_git_config(
-  user.name = "Joseny Oliveira"
-  user.email = "josenyds@gmail.com")
-)
+
+## Passo 2: Criando o repositorio Github Local
+usethis::use_git()
+
+
+## Passo 3: Inserindo arquivos ao repositorio
+
+##Passo 4: Criando/inserindo ao repositorio Github (Nuvens)
+usethis::use_github()
